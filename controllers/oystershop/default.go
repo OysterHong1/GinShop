ec618a783b10682175a365bbc8ee3a6028cfde22
Oyster181 <20223903013@m.scnu.edu.cn>
2024-11-13 21:56:48 +0800

    配置了redis缓存服务,为商城首页部分数据配置了redis缓存，提高了运行速度

--- controllers/oystershop/default.go
+++ controllers/oystershop/default.go
@@ -2,11 +2,9 @@ package oystershop
 
 import (
 	"GinShop/models"
+	"fmt"
 	"github.com/gin-gonic/gin"
-	. "github.com/hunterhug/go_image"
-	"github.com/skip2/go-qrcode"
 	"gorm.io/gorm"
-	"io/ioutil"
 	"net/http"
 	"strings"
 )
@@ -16,20 +14,38 @@ type DefaultController struct{}
 func (con DefaultController) Index(c *gin.Context) {
 	//1、获取顶部导航
 	topNavList := []models.Nav{}
-	models.DB.Where("status=1 AND position=1").Find(&topNavList)
+	if hasTopNavList := models.CacheDb.Get("topNavList", &topNavList); !hasTopNavList {
+		/*redis中若没有数据，则从sql数据库之中查,随后再缓存到redis之中*/
+		models.DB.Where("status=1 AND position=1").Find(&topNavList)
+		models.CacheDb.Set("topNavList", topNavList, 60*60)
+		fmt.Println("正在从Mysql读取数据")
+	} else {
+		fmt.Println("正在从redis读取数据")
+	}
+
 	//2、获取轮播图数据
 	focusList := []models.Focus{}
-	models.DB.Where("status=1 AND focus_type=1").Find(&focusList)
+	if hasFocusList := models.CacheDb.Get("focusList", &focusList); !hasFocusList {
+		models.DB.Where("status=1 AND focus_type=1").Find(&focusList)
+		models.CacheDb.Set("focusList", focusList, 60*60)
+	}
+
 	//3、获取分类的数据
 	goodsCateList := []models.GoodsCate{}
 	//https://gorm.io/zh_CN/docs/preload.html
-	models.DB.Where("pid = 0 AND status=1").Order("sort DESC").Preload("GoodsCateItems", func(db *gorm.DB) *gorm.DB {
-		return db.Where("goods_cate.status=1").Order("goods_cate.sort DESC")
-	}).Find(&goodsCateList)
+	if hasCateList := models.CacheDb.Get("goodsCateList", &goodsCateList); !hasCateList {
+		models.DB.Where("pid = 0 AND status=1").Order("sort DESC").Preload("GoodsCateItems", func(db *gorm.DB) *gorm.DB {
+			return db.Where("goods_cate.status=1").Order("goods_cate.sort DESC")
+		}).Find(&goodsCateList)
+		models.CacheDb.Set("goodsCateList", goodsCateList, 60*60)
+	}
 
 	//4、获取中间导航
 	middleNavList := []models.Nav{}
-	models.DB.Where("status=1 AND position=2").Find(&middleNavList)
+	if hasmiddleNavList := models.CacheDb.Get("middleNavList", &middleNavList); !hasmiddleNavList {
+		models.DB.Where("status=1 AND position=2").Find(&middleNavList)
+		models.CacheDb.Set("middleNavList", middleNavList, 60*60)
+	}
 
 	for i := 0; i < len(middleNavList); i++ {
 		relation := strings.ReplaceAll(middleNavList[i].Relation, "，", ",") //21，22,23,24
@@ -40,12 +56,18 @@ func (con DefaultController) Index(c *gin.Context) {
 	}
 
 	//手机
-
-	phoneList := models.GetGoodsByCategory(1, "best", 8)
+	phoneList := []models.Goods{}
+	if hasPhoneList := models.CacheDb.Get("phoneList", &phoneList); !hasPhoneList {
+		phoneList := models.GetGoodsByCategory(1, "best", 8)
+		models.CacheDb.Set("phoneList", phoneList, 60*60)
+	}
 
 	//配件
-
-	otherList := models.GetGoodsByCategory(9, "all", 1)
+	otherList := []models.Goods{}
+	if hasOtherList := models.CacheDb.Get("otherList", &otherList); !hasOtherList {
+		otherList := models.GetGoodsByCategory(9, "all", 8)
+		models.CacheDb.Set("otherList", otherList, 60*60)
+	}
 
 	c.HTML(http.StatusOK, "oystershop/index/index.html", gin.H{
 		"topNavList":    topNavList,
@@ -55,49 +77,4 @@ func (con DefaultController) Index(c *gin.Context) {
 		"phoneList":     phoneList,
 		"otherList":     otherList,
 	})
-
-}
-
-func (con DefaultController) Thumbnail1(c *gin.Context) {
-	//宽度缩放
-	fileName := "static/admin/imageResources/1631938291.png"
-	savePath := "static/upload/1631938291_pro.png"
-	err := ScaleF2F(fileName, savePath, 600)
-	if err != nil {
-		c.String(200, "生成图片失败")
-		return
-	}
-	c.String(200, "Thumbnail1 成功")
-}
-
-func (con DefaultController) Thumbnail2(c *gin.Context) {
-	//按宽度与高度比例缩放
-	fileName := "static/admin/imageResources/1631938291.png"
-	savePath := "static/upload/1631938291_Thumb400.png"
-	//参数不等时进行剪切
-	err := ThumbnailF2F(fileName, savePath, 400, 400)
-	if err != nil {
-		c.String(200, "生成图片失败")
-		return
-	}
-	c.String(200, "Thumbnail2 成功")
-}
-
-func (con DefaultController) Qrcode1(c *gin.Context) {
-	var png []byte
-	png, err := qrcode.Encode("https://www.hjh.com", qrcode.Medium, 256)
-	if err != nil {
-		c.String(200, "二维码生成失败")
-	}
-	c.String(200, string(png))
-}
-
-func (con DefaultController) Qrcode2(c *gin.Context) {
-	savePath := "static/upload/qrcode2.png"
-	err := qrcode.WriteFile("https://www.hjh.com", qrcode.Medium, 556, savePath)
-	if err != nil {
-		c.String(200, "生成二维码失败")
-	}
-	file, _ := ioutil.ReadFile(savePath)
-	c.String(200, string(file))
 }
